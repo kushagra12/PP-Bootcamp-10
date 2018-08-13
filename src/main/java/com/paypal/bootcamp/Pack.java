8bb8113f76de12902e8df9dc374fcb9937a648b7
Ranganathan <sanranganathan@lm-maa-22004789.paypalcorp.com>
2018-08-13 13:28:26 +0530

    defined Pack

--- src/main/java/com/paypal/bootcamp/Pack.java
+++ src/main/java/com/paypal/bootcamp/Pack.java
@@ -1,4 +1,29 @@
 package com.paypal.bootcamp;
 
-public class Pack {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Pack{
+
+    List<Card> cards;
+
+    public boolean isJoker (Pip p, Suit s){
+        return p.equals(Pip.JOKER) || s.equals(Suit.JOKER);
+    }
+
+    public Pack(boolean containsJoker){
+        cards = new ArrayList<Card>();
+        for (Pip p : Pip.values()){
+            for (Suit s : Suit.values()){
+                if (!isJoker(p, s)){
+                    cards.add(new Card(s, p));
+                }
+            }
+        }
+        if (containsJoker) {
+            cards.add(new Card(Suit.JOKER, Pip.JOKER));
+            cards.add(new Card(Suit.JOKER, Pip.JOKER));
+        }
+    }
 }
