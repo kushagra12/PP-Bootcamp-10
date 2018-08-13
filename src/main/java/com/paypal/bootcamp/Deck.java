b5dab8e6b70f2f0391c54abc57fd11d5fadac011
sidd607 <sidd607@gmail.com>
2018-08-13 13:26:44 +0530

    Deck

--- src/main/java/com/paypal/bootcamp/Deck.java
+++ src/main/java/com/paypal/bootcamp/Deck.java
@@ -1,4 +1,32 @@
 package com.paypal.bootcamp;
 
+import java.util.Collections;
+import java.util.List;
+
 public class Deck {
+
+    List<Card> cards;
+
+    Deck (Integer numberPacks, boolean withJokers) {
+
+        for (int i = 0; i < numberPacks; i++) {
+            cards.addAll(new Pack(withJokers).getCards());
+        }
+
+    }
+
+    public void shuffle (){
+        Collections.shuffle(this.cards);
+    }
+
+    public List<Card> getNCards (Integer n) {
+        if ( n > cards.size())
+            n = cards.size()-1;
+
+        List<Card> nCards =  cards.subList(0, n);
+        cards.removeAll(nCards);
+        return nCards;
+
+    }
+
 }
