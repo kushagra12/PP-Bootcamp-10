c5aea0a403374d988561deadd651666356618e85
kushagra12 <vaish.kushagra@gmail.com>
2018-08-13 12:15:02 +0530

    Defined basic card structure

--- src/main/java/com/paypal/bootcamp/Card.java
+++ src/main/java/com/paypal/bootcamp/Card.java
@@ -1,7 +1,33 @@
 package com.paypal.bootcamp;
 
-public abstract class Card implements Comparable<Card>{
-    private Suit suit;
-    private Pip pip;
+public abstract class Card{
+    Suit suit;
+    Pip pip;
 
+    public Suit getSuit() {
+        return suit;
+    }
+
+    public void setSuit(Suit suit) {
+        this.suit = suit;
+    }
+
+    public Pip getPip() {
+        return pip;
+    }
+
+    public void setPip(Pip pip) {
+        this.pip = pip;
+    }
+
+    public Card(Suit suit, Pip pip) {
+        this.suit = suit;
+        this.pip = pip;
+    }
+
+    public boolean equals(Card card) {
+        return card.pip == this.pip && card.suit == this.suit;
+    }
+
+    public abstract int getValue();
 }
